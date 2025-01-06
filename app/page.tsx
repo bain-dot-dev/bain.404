04a5726e9fe5bd61bc1ecd5327b2d3c661a9a475
Bain Hansly Cruz <repatobain@gmail.com>
2025-01-06 18:29:11 +0800

    import naming fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 import Footer from "@/components/footer";
-import Section1 from "@/components/hero";
-import Section2 from "@/components/section1";
-import Section3 from "@/components/section2";
-import Section4 from "@/components/section3";
+import Hero from "@/components/hero";
+import Section1 from "@/components/section1";
+import Section2 from "@/components/section2";
+import Section3 from "@/components/section3";
 
 export default function Page() {
   return (
     <div className="min-h-screen bg-customColors-pastelWhite dark:bg-customColors-woodSmoke">
+      <Hero />
       <Section1 />
       <Section2 />
       <Section3 />
-      <Section4 />
       <Footer />
     </div>
   );
