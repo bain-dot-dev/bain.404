504bf824d356c26a069cb1e75f2eb9a5c6a17f3e
Bain Hansly Cruz <repatobain@gmail.com>
2025-07-03 22:29:43 +0800

    add project details and images for Task It Task Master, Tic Tac Toe, and Facebook Wall Concept 2008; update resume

--- data/projects.ts
+++ data/projects.ts
@@ -8,7 +8,7 @@ export const projects: Project[] = [
       "Enchanté is a Filipino beauty brand offering a curated collection of high-quality, stylish lipsticks designed to elevate your everyday look.",
     image: "/projects/enchante/enchante.png",
     featured: true,
-    // github: "#",
+    github: "https://github.com/bain-dot-dev/enchante",
     liveUrl: "https://bain-dot-dev.github.io/enchante/",
   },
   {
@@ -19,7 +19,7 @@ export const projects: Project[] = [
     image: "/projects/3rshaneims/3rshaneims.png",
     featured: true,
     // github: "#",
-    liveUrl: "https://youtu.be/16PLR-7YK0Y",
+    // liveUrl: "https://youtu.be/16PLR-7YK0Y",
   },
   {
     id: "Travlog",
@@ -28,7 +28,7 @@ export const projects: Project[] = [
       "Travelog is a modern travel platform offering destination insights, tour services, and easy booking options for travelers.",
     image: "/projects/travlog/travlog.png",
     featured: true,
-    // github: "#",
+    github: "https://github.com/bain-dot-dev/travelog",
     liveUrl: "https://travelog-xi.vercel.app/",
   },
   {
@@ -38,5 +38,38 @@ export const projects: Project[] = [
       "A scheduling app that manages academic schedules for faculty and students while optimizing room utilization and preventing conflicts.",
     image: "/projects/schedlr/schedlr.png",
     featured: true,
+    // github: "#",
+    // liveUrl: "#",
+  },
+  {
+    id: "Task It Task Master",
+    title: "Task It Task Master",
+    description:
+      "Task It Task Master is a task management tool that helps users organize and prioritize their tasks effectively.",
+    image: "/projects/task-it-task-master/task-it-task-master.png",
+    featured: true,
+    github: "https://github.com/bain-dot-dev/ascendens-assessment",
+    // liveUrl: "#",
+  },
+  {
+    id: "Tic-Tac-Toe",
+    title: "Tic Tac Toe",
+    description:
+      "Tic Tac Toe is a classic game that allows users to play against each other or an AI opponent.",
+    image: "/projects/tic-tac-toe/tic-tac-toe.png",
+    featured: true,
+    github: "https://github.com/bain-dot-dev/tic-tac-toe-game",
+    liveUrl: "https://tic-tac-toe-game-beta-bay.vercel.app/",
+  },
+  {
+    id: "Facebook Wall Concept 2008",
+    title: "Facebook Wall Concept 2008",
+    description:
+      "A conceptual design for the Facebook wall feature, showcasing user interactions and post displays.",
+    image:
+      "/projects/facebook-wall-concept-2008/facebook-wall-concept-2008.png",
+    featured: true,
+    github: "https://github.com/bain-dot-dev/facebook-wall-concept-2008",
+    liveUrl: "https://facebook-wall-concept-2008.vercel.app/",
   },
 ];
